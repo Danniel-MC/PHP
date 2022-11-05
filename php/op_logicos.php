fb9830ba1ea3e1761be25d358fc2abbdbec43cbd
danniel-mc <enriquenavarro315@gmail.com>
2022-11-05 04:37:37 -0600

    op_logicos

---
+++ php/op_logicos.php
@@ -0,0 +1,43 @@
+<?php
+// ? Operadores logicos
+$x = true;
+$y = false;
+
+echo 'x : ' . $x . ' y : ' . $y;
+
+// and &&
+echo "<br />";
+echo "<br />";
+echo "AND &&";
+
+echo  $x and $y;
+echo "<br />";
+echo  $x && $y;
+echo "<br />";
+
+// or, ||
+echo 'OR ||';
+echo "<br />";
+
+echo  $x or $y;
+echo "<br />";
+echo  $x || $y;
+echo "<br />";
+
+// xor
+echo '<br />';
+echo 'XOR';
+
+echo '<br />';
+echo  $x xor $y;
+
+// negacion 
+echo '<br />';
+echo 'negacion !';
+
+echo '<br />';
+echo 'x' . !$x;
+echo '<br />';
+echo 'y' . !$y;
+
+

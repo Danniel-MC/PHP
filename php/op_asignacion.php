8e942a10c8755ddc3ad2903acd164dae35ff33af
danniel-mc <enriquenavarro315@gmail.com>
2022-11-05 04:58:32 -0600

    op_asignacion

---
+++ php/op_asignacion.php
@@ -0,0 +1,27 @@
+<?php
+$a =4.5;
+$b = 5;
+echo 'a: ' . $a . ' b: ' . $b . '<br />';
+
+// suma con asignacion
+// manera larga: $x = $a + $b;
+$a += $b;
+echo '$a += $b = ' . $a;
+echo '<br />';
+// resta con asignacion
+$a -= $b;
+echo '$a -= $b = ' . $a;
+echo '<br />';
+// multi con asignacion
+$a *= $b;
+echo '$a *= $b = ' . $a;
+echo '<br />';
+// fiv con asignacion
+$a /= $b;
+echo '$a /= $b = ' . $a;
+echo '<br />';
+// modulo con asignacion
+$a %= $b;
+echo '$a %= $b = ' . $a;
+echo '<br />';
+

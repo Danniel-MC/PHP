e54cc524d2673bd1ac195a24f396e0789eb68fb7
danniel-mc <enriquenavarro315@gmail.com>
2022-11-04 19:46:19 -0600

    Vistazo a constanes

---
+++ php/constantes.php
@@ -0,0 +1,11 @@
+<?php
+//? Las constantes se definen en mayus por convecion. 
+
+define('PULGADA', 0.3970);
+define('LIBRA', 2.204);
+
+$cm  = 50;
+$kilo = 10;
+
+echo PULGADA . " Cm a Pulg = " . $cm * PULGADA . " PULG <br />";
+echo LIBRA . " Lib a Kilo = " . $kilo * LIBRA . " Kilos <br />";
---
+++ php/variables.php
@@ -0,0 +1,3 @@
+<?php
+$name = "Test";
+echo $name;

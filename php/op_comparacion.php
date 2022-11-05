fd12e1b6a14361029c34b4dee64502beffd31086
danniel-mc <enriquenavarro315@gmail.com>
2022-11-05 04:47:50 -0600

    op_comparacion

---
+++ php/op_comparacion.php
@@ -0,0 +1,22 @@
+<?php
+$x = 10;
+$b = 20;
+
+echo 'x=' . $x . ' ' . 'b=' . $b;
+echo '<br />';
+
+//comparacion
+echo $x == $b;
+echo '<br />';
+
+// estricto
+echo $x === $b;
+echo '<br />';
+
+// distinto
+echo $x != $b;
+echo '<br />';
+
+// sino son iguales
+echo $x !== $b;
+echo '<br />';
